232b2773e6434b3d39e996028788938df9b95564
Greg Kitson <greg.kitson@gmail.com>
2014-07-16 09:19:48 -0600

    notify pe-httpd when changing pe_console_timeout

--- site/profile/manifests/puppet/console.pp
+++ site/profile/manifests/puppet/console.pp
@@ -1,5 +1,7 @@
 class profile::puppet::console {
+  include '::request_manager'
   class { '::pe_console_timeout':
     timeout_interval => '3600',
+    notify           => Service['pe-httpd'],
   }
 }
