77b815a3e409de85d4817802c9f43c7d52704df9
Greg Kitson <greg.kitson@gmail.com>
2014-07-17 18:37:57 -0600

    add file_line root bash_profile PATH to profile::base

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,3 +1,8 @@
 class profile::base {
   include '::ntp'
+  file_line { 'root bash_profile PATH':
+    path  => '/root/.bash_profile',
+    line  => 'PATH=$HOME/bin:$PATH:/opt/puppet/bin',
+    match => 'PATH=',
+  }
 }
