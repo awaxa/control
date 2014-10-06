fc9c3ebfa9157d17f2658654556cac821fb53c24
Greg Kitson <greg.kitson@gmail.com>
2014-10-06 00:30:46 -0700

    add puppetlabs apt source

--- site/profile/manifests/workstation.pp
+++ site/profile/manifests/workstation.pp
@@ -2,6 +2,12 @@ class profile::workstation {
   class { 'apt':
     purge_sources_list_d => true,
   }
+  apt::source { 'puppetlabs':
+    location   => 'http://apt.puppetlabs.com',
+    repos      => 'main',
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+  }
 
   package { 'ubuntu-desktop':
     ensure => present,
