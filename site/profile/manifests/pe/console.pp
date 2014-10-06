6d9104f0716e596aff7be5927145e8d8506dd2bc
Greg Kitson <greg.kitson@gmail.com>
2014-10-05 23:00:42 -0700

    remove request_manager include from profile::pe::console

--- site/profile/manifests/pe/console.pp
+++ site/profile/manifests/pe/console.pp
@@ -1,8 +1,6 @@
 class profile::pe::console {
-  include request_manager
   class { 'pe_console_timeout':
     timeout_interval => '3600',
-    notify           => Service['pe-httpd'],
   }
   firewall { '100 allow https access':
     port   => '443',
