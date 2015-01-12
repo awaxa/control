45ea2cc97b2c1aa32c5afb7c4011dfa4e69bed60
Greg Kitson <greg.kitson@gmail.com>
2015-01-11 16:04:29 -0800

    Revert "this is unnecessary"
    This reverts commit afc865b4a96b4c1238fb63f5a456e5caf415c163.

--- scripts/bootstrap_r10k.sh
+++ scripts/bootstrap_r10k.sh
@@ -5,6 +5,8 @@
 gem install r10k --no-rdoc --no-ri -v 1.4.0
 PUPPETFILE=/tmp/control/Puppetfile PUPPETFILE_DIR=/tmp/control/modules r10k puppetfile install --verbose
 puppet apply /tmp/control/manifests --modulepath='/tmp/control/site:/tmp/control/modules:/opt/puppet/share/puppet/modules'
+rm -rfv "$(puppet config print confdir)/environments"
+mkdir -pv "$(puppet config print confdir)/environments"
 r10k deploy environment -vp
 
 mkdir -p /etc/puppetlabs/facter/facts.d
